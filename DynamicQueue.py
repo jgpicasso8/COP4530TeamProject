68e7c1839f227a5506496685db7e547150d2b5cd
Josue Gonzalez <66024359+jgpicasso8@users.noreply.github.com>
2020-08-24 23:03:01 -0400

    Update DynamicQueue.py

--- DynamicQueue.py
+++ DynamicQueue.py
@@ -1,4 +1,3 @@
-
 # Authors: Andres Greene
 #         Raquel Garcia
 #         Laureano Griffin
@@ -9,10 +8,7 @@ class Dynamic_queue:
     # Constructor
     def __init__(self, value=10):
         # Initializes attributes
-        self.elements = []*value
-        # self.iHead = 0
-        # self.iTail = 0
-        # self.entryCount = 0
+        self.elements = [None]*value
         # Sets capacity equal to 1 if the value is equal to zero or lower
         if value <= 0:
             self.currCapacity, self.initialCapacity = 1, 1
@@ -27,7 +23,7 @@ class Dynamic_queue:
     # Copy Constructor - creates a new instance of queue
     def __copy__(self):
         copy = Dynamic_queue(self.currCapacity)
-        if len(self.elements) == 0:
+        if self.size() == 0:
             return copy
         else:
             for x in self.elements:
@@ -35,7 +31,7 @@ class Dynamic_queue:
         return copy
 
     # Accessors - access the queue, but doesn't modify it
-    # Returns the object at the head of the queue
+    # Returns the object at the head of the queue - CHANGE THIS
     def head(self):
         if len(self.elements) == 0:
             return "empty"
@@ -44,11 +40,15 @@ class Dynamic_queue:
 
     # Returns the number of objects currently stored in the queue
     def size(self):
-        return len(self.elements)
+        count = 0
+        for x in self.elements:
+            if x is not None:
+                count += 1
+        return count
 
     # Returns true if the queue is empty. Returns false otherwise
     def empty(self):
-        if len(self.elements) == 0:
+        if self.elements[0] is None:
             return True
         else:
             return False
@@ -60,9 +60,9 @@ class Dynamic_queue:
     # Mutators - Changes values in the queue
     # will swap all the member variables of this queue with those of the argument
     def swap(self, swapper):
-        for x in self.elements:
-            self.elements.pop(0)
-            self.elements.append(swapper)
+        for x in range(len(self.elements)):
+            if self.elements[x] is not None:
+                self.elements[x] = swapper
         return self.elements
 
     # Swaps member variables with the copy on the right side of the operator
@@ -80,23 +80,50 @@ class Dynamic_queue:
     # Insert the argument at the end of the queue.
     # If the array is full before the argument is placed, the array is doubled first.
     def enqueue(self, value):
+        # Adds when queue is full & doubles the size
         if self.currCapacity == self.size():
             self.currCapacity *= 2
-        self.elements.append(value)
+            newList = [None] * self.currCapacity
+            for x in range(len(self.elements)):
+                if newList[x] is None:
+                    newList[x] = self.elements[x]
+            self.elements = newList
+            for x in range(len(self.elements)):
+                if self.elements[x] is None:
+                    self.elements[x] = value
+                    break
+        # Add regularly
+        else:
+            for x in range(len(self.elements)):
+                if self.elements[x] is None:
+                    self.elements[x] = value
+                    break
         return
 
     # Removes the object at the front of the queue. If, after the object is removed, the array is
     # 1/4 full, and its greater than the initial capacity, the capacity is halved.
     def dequeue(self):
-        self.elements.pop(0)
-        if self.size == 0:
+        # Returns if size or else will get error
+        if self.size() == 0:
             return
-        elif (self.capacity() > self.initialCapacity) & (self.size()/self.capacity() <= 0.25):
+        self.elements.pop(0) # Pop the thingy otherwise
+        self.elements.append(None)
+        # 1/4 full, and its greater than the initial capacity, the capacity is halved.
+        if (self.capacity() > self.initialCapacity) & (self.size()/self.capacity() <= 0.25):
             self.currCapacity /= 2
             self.currCapacity = int(self.currCapacity)
+            newList = [None] * self.currCapacity
+            i = 0
+            while i < self.currCapacity:
+                newList[i] = self.elements[i]
+                i += 1
+            self.elements = newList
+
         return
 
     # Resets member variables to empty queue. The array is resized to the initial capacity
     def clear(self):
         self.elements.clear()
+        for i in range(self.initialCapacity):
+            self.elements.append(None)
         return self.elements
